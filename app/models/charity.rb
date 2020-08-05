4fae31cb86eddbfdd4a03f2c19401aa2e594049a
Luis Novoa <luis.matte.novoa@gmail.com>
2020-08-05 16:57:11 -0400

    Corrects charity#credit_amount method

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -2,6 +2,6 @@ class Charity < ActiveRecord::Base
   validates :name, presence: true
 
   def credit_amount(amount)
-    update_column :total, total + amount
+    update_attribute :total, total + amount
   end
 end
